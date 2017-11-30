a2c901f32451494f31893cc983516de6311dbf32
Edgar Albalate <dev.gardo@gmail.com>
2017-11-30 18:25:30 +0100

    Added some commands:   /add   /title

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const TeleBot = require('telebot');
-const fs = require('fs');
+const TeleBot = require('telebot')
+const fs = require('fs')
 
-let token = fs.readFileSync("token.txt", 'utf8');
-
+let token
+try{
+  token = fs.readFileSync("token.txt", 'utf8')
+  token = token.replace('\n', '')
+} catch(e) {
+  if(e.code == 'ENOENT')
+    console.log("  Error: token.txt not found")
+  else console.log(e)
+  process.exit(1)
+}
 const bot = new TeleBot({
   token: token,
   polling: {limit: 50}
-});
+})
 
-bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
+bot.on(['/start', '/hello'], (msg) => {
+  if(msg.chat.type != 'supergroup')
+    return msg.reply.text("Sorry, I only work with supergroups")
+  msg.reply.text('Hola! He llegado para gestionar los mensajes anclados')
+  msg.reply.text(msg.text.slice(7) || 'Untitled').then( d => {
+    bot.pinChatMessage(d.result.chat.id, d.result.message_id)
+  })
+})
 
-bot.on('text', (msg) => msg.reply.text(msg.text));
+bot.on(['/add', '/anclar'], require('./lib/cmd_add')(bot) )
 
-bot.start();
+bot.on(['/title', '/titulo'], (msg) => {
+  
+})
+
+bot.start()
